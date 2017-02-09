5f6eaf1f95a22925f18dd1a4657930adbf6e80e7
Aurel Wildfellner <aurel.w@gmail.com>
2017-02-09 23:58:52 +0100

    the script

---
+++ lolwebstatus.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler,HTTPServer
+
+import paho.mqtt.client as paho
+
+
+spaceOpen = False
+brokerHost = "mqtt.devlol.org"
+mqttTopic = "devlol/h19/door/lockstatus"
+
+
+class MyHandler(BaseHTTPRequestHandler):
+
+     def do_HEAD(client):
+        client.send_response(200)
+        client.send_header("Content-type", "text/html")
+        client.end_headers()
+
+     def do_GET(self):
+        global spaceOpen
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "image/jpeg")
+            self.end_headers()
+            if spaceOpen:
+                print("return OPEN")
+                #self.wfile.write(load('open.html'))
+                try:
+                    self.wfile.write(load_binary('open.jpg'))
+                except BrokenPipeError:
+                    pass
+            else:
+                print("return CLOSED")
+                #self.wfile.write(load('closed.html'))
+                try:
+                    self.wfile.write(load_binary('closed.jpg'))
+                except BrokenPipeError:
+                    pass
+
+
+def load_binary(file):
+    with open(file, 'rb') as file:
+        return file.read()
+
+def load(file):
+    with open(file, 'r') as file:
+        return encode(str(file.read()))
+
+def encode(file):
+    return bytes(file, 'UTF-8')
+
+
+
+def on_message(client, userdata, msg):
+    global spaceOpen
+    payload = msg.payload.decode("utf-8")
+    print(payload)
+    """ Callback for mqtt message."""
+    if mqttTopic == msg.topic:
+        if payload == "UNLOCKED":
+            spaceOpen = True
+            print("UNLOCKED")
+        elif payload == "LOCKED":
+            spaceOpen = False
+            print("LOCKED")
+
+
+def on_disconnect(client, userdata, foo):
+    connected = False
+    while not connected:
+        try:
+            client.reconnect()
+            connected = True
+            # resubscribe to the topics
+            client.subscribe(mqttTopic)
+        except:
+            print("Failed to reconnect...")
+            time.sleep(1)
+
+
+
+def run(server_class=HTTPServer,
+        handler_class=MyHandler):
+
+    ## setup MQTT client
+    client = paho.Client()
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+
+    try:
+        client.connect(brokerHost)
+    except:
+        print("failed to connect")
+        on_disconnect(client, None, None)
+
+    client.subscribe(mqttTopic)
+
+
+    server_address = ('', 8321)
+    httpd = server_class(server_address, handler_class)
+
+    while True:
+        httpd.handle_request()
+        client.loop()
+
+if __name__ == "__main__":
+    run()
